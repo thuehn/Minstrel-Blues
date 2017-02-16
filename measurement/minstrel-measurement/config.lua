2b46a4b54fa44ea315066dd713b820e8dbe8a42c
Denis Roeper <denis.roeper@gmx.de>
2017-02-16 20:33:17 +0100

    update config file to new format

--- measurement/minstrel-measurement/config.lua
+++ measurement/minstrel-measurement/config.lua
@@ -1,35 +1,10 @@
-aps = { 
-    { name = "lede-ap"
-    , radio = "radio0"
-    , ctrl_if = "br-lan"
-    }
-}
 
-stations = {
-    { name = "lede-sta"
-    , radio = "radio0"
-    , ctrl_if = "eth0"
-    },
+ctrl = "lede-ctrl"
 
-    { name = "lede-ctrl"
-    , radio = "radio0"
-    , ctrl_if = "eth0"
-    }
-}
+connections["lede-ap"] = { "lede-sta" }
 
-mesh = {
-    { name = "lede-ap"
-    , radio = "radio0"
-    , ctrl_if = "br-lan"
-    },
-
-    { name = "lede-sta"
-    , radio = "radio0"
-    , ctrl_if = "eth0"
-    },
-
-    { name = "lede-ctrl"
-    , radio = "radio0"
-    , ctrl_if = "eth0"
-    }
+nodes = {
+    { name = "lede-ap", radio = "radio0", ctrl_if = "br-lan" },
+    { name = "lede-sta", radio = "radio0", ctrl_if = "eth0"  },
+    { name = "lede-ctrl", radio = "radio0", ctrl_if = "eth0" }
 }
