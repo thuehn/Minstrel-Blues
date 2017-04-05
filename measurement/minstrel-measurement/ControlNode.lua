a349f2a10f5f63f0cc33504313ae24a684487085
Denis Roeper <denis.roeper@gmx.de>
2017-04-05 19:56:31 +0200

    fix: printing tablenot string

--- measurement/minstrel-measurement/ControlNode.lua
+++ measurement/minstrel-measurement/ControlNode.lua
@@ -563,15 +563,15 @@ function ControlNode:run_experiment ( command, args, ap_names, is_fixed, key, nu
 
         local rates = ap_ref.rpc.tx_rate_indices ( ap_ref.wifi_cur, ap_ref.stations[1] )
         local msg = "rate indices: "
-        self:send_debug ( msg .. table_tostring ( rates, 80  - string.len ( msg ) ) )
+        self:send_debug ( msg .. table_tostring ( rates, 80 - string.len ( msg ) ) )
 
         local powers = ap_ref.rpc.tx_power_indices ( ap_ref.wifi_cur, ap_ref.stations[1] )
         local msg = "power indices: "
-        self:send_debug ( msg .. table_tostring ( powers, 80  - string.len ( msg ) ) )
+        self:send_debug ( msg .. table_tostring ( powers, 80 - string.len ( msg ) ) )
 
         local iw_info = ap_ref.rpc.get_iw_info ( ap_ref.wifi_cur )
         local msg = "iw info: "
-        self:send_info ( msg .. iw_info, 80  - string.len ( msg ) )
+        self:send_info ( msg .. ( iw_info or "none" ), 80 - string.len ( msg ) )
 
         for i, sta_ref in ipairs ( ap_ref.refs ) do
 
@@ -579,7 +579,8 @@ function ControlNode:run_experiment ( command, args, ap_names, is_fixed, key, nu
 
                 local iw_link = sta_ref.rpc.get_iw_link ( sta_ref.wifi_cur )
                 local msg = "iw link: "
-                self:send_info ( msg .. iw_link, 80  - string.len ( msg ) )
+                -- fixme: should return string, not table value
+                self:send_info ( msg .. table_tostring ( iw_link ), 80 - string.len ( msg ) )
                 
                 local rate_name = sta_ref.rpc.get_linked_rate_idx ( sta_ref.wifi_cur )
                 if ( rate_name ~= nil ) then
